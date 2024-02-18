36532ddc69cee9c9425310f59fbf0a283b89fa70
Leonel Baltazar Bravo <96897286+pybalt@users.noreply.github.com>
2024-02-18 09:34:56 -0300

    Remove unused imports in api_views.py

--- app/api/api_views.py
+++ app/api/api_views.py
@@ -1,7 +1,5 @@
-from datetime import date
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.pagination import PageNumberPagination
 from rest_framework import status
 from drf_yasg.utils import swagger_auto_schema
 
